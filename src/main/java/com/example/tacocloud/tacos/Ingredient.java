d7577205e7c158bd06594130f2a25313ce91b97c
Mustaphin.M <mustafin.m@mintrud.by>
2024-06-13 21:14:37 +0300

    Глава 2

---
+++ src/main/java/com/example/tacocloud/tacos/Ingredient.java
@@ -0,0 +1,19 @@
+package com.example.tacocloud.tacos;
+
+import lombok.Data;
+
+/**
+ *
+ * @author marat
+ */
+@Data
+public class Ingredient {
+
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+}
